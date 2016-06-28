e6b92ba3378d1f13b7f5ddac62d4fb08be8834d3
Ricardo Casaca <ricardocasaca33@gmail.com>
2016-06-28 09:38:35 -0300

    Refatorando função main.

--- src/main/java/com/ricardo/main/Main.java
+++ src/main/java/com/ricardo/main/Main.java
@@ -2,13 +2,11 @@ package com.ricardo.main;
 
 import com.ricardo.conexao.EntityManagerFactorySingleton;
 import com.ricardo.interfaces.AutenticacaoService;
-import com.ricardo.interfaces.ConexaoHandler;
 import com.ricardo.interfaces.PromptService;
 import com.ricardo.interfaces.UsuarioService;
 import com.ricardo.servicos.AutenticacaoServiceImpl;
 import com.ricardo.servicos.PromptServiceImpl;
 import com.ricardo.servicos.UsuarioServiceImpl;
-import com.ricardo.tela.CadastroUsuario;
 import com.ricardo.tela.GerenciamentoDeTela;
 import com.ricardo.tela.Login;
 import com.ricardo.util.UserInput;
@@ -17,28 +15,34 @@ class Main {
 
     public static void main(String[] args) {
         EntityManagerFactorySingleton.getInstance();
-        UsuarioService usuarioService = new UsuarioServiceImpl();
-
-        if (!existeAdminCadastrado(usuarioService)) {
-            System.out.println("Nenhum usuário administrador cadastrado na base. É preciso cadastrar pelo menos 1 administrador\n");
-            cadastraUsuarioAdmin();
-        }
-
-        PromptService promptService = new PromptServiceImpl(new UserInput());
-        AutenticacaoService autenticacaoService = new AutenticacaoServiceImpl(usuarioService);
-        GerenciamentoDeTela gT = new GerenciamentoDeTela();
-
-        GerenciamentoDeTela gerenciamentoDeTela = new GerenciamentoDeTela(new Login(promptService, autenticacaoService, usuarioService, gT));
-        gerenciamentoDeTela.exibirTela();
+        cadastraAdminSeNaoHouver();
+        exibeTelaDeLogin();
 
         System.exit(0);
     }
 
-    public static boolean existeAdminCadastrado(UsuarioService uS){
+    public static boolean existeAdminCadastrado(){
+        UsuarioService uS = new UsuarioServiceImpl();
         return uS.existeAdmin();
     }
 
     public static void cadastraUsuarioAdmin(){
         new com.ricardo.funcoesmenu.CadastroUsuario().executar();
+    }
+
+    public static void cadastraAdminSeNaoHouver(){
+        if (!existeAdminCadastrado()) {
+            System.out.println("Nenhum usuário administrador cadastrado na base. É preciso cadastrar pelo menos 1 administrador\n");
+            cadastraUsuarioAdmin();
+        }
+    }
+
+    public static void exibeTelaDeLogin(){
+        UsuarioService uS = new UsuarioServiceImpl();
+        PromptService pS = new PromptServiceImpl(new UserInput());
+        AutenticacaoService aS = new AutenticacaoServiceImpl(uS);
+        GerenciamentoDeTela gT = new GerenciamentoDeTela();
+        GerenciamentoDeTela gerenciamentoDeTela = new GerenciamentoDeTela(new Login(pS, aS, uS, gT));
+        gerenciamentoDeTela.exibirTela();
     }
 }
